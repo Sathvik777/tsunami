08a25f791a7e8baa9e300c1c5b905e098a8c9e24
Sathvik777 <katam.sathvik@gmail.com>
2020-01-19 15:15:32 +0100

    feat(ml-train): Add basic code to train a model with LinearRegression

--- data-extract/main.go
+++ data-extract/main.go
@@ -9,10 +9,10 @@ import (
 // URL is Prometheus api endpoint for query
 const URL = "http://localhost:9090/api/v1/query?query=%s"
 
-// Memory Query
+// PROMQL1 query for Memory Query
 const PROMQL1 = "{'query':'(node_memory_MemTotal_bytes - (node_memory_MemFree_bytes + node_memory_Cached_bytes + node_memory_Buffers_bytes)) / node_memory_MemTotal_bytes * 100'}"
 
-// CPU Query
+// PROMQL2 query for  CPU Query
 const PROMQL2 = "{'query':'100 - avg(irate(node_cpu_seconds_total{job='node',mode='idle'}[5m])) by (instance) * 100'}"
 
 func main() {
